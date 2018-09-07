1148e5926db6094d727a5f1c5b0f06e671257e9a
TE96 <odtuntun@gmail.com>
2018-09-07 09:08:35 +0800

    all files

---
+++ snippets/serializers.py
@@ -0,0 +1,64 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from snippets.models import Snippet
+# from snippets.models import Snippet, LANGUAGES_CHOICE, STYLE_CHOICE
+
+
+# class SnippetSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     title = serializers.CharField(
+#         required=False, allow_blank=True, max_length=100)
+#     code = serializers.CharField(style={'base_template': 'testarea.html'})
+#     linenos = serializers.BooleanField(required=False)
+#     language = serializers.ChoiceField(
+#         choices=LANGUAGES_CHOICE, default='python')
+#     style = serializers.ChoiceField(choices=STYLE_CHOICE, default='friendly')
+class SnippetSerializer(serializers.ModelSerializer):
+
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Snippet
+        fields = ('id', 'title', 'code', 'linenos',
+                  'language', 'style', 'owner')
+
+    def create(self, validated_data):
+        """
+        Create and return a new `Snippet instance, given the validated data.
+
+        Args:
+            validated_data (TYPE): Description
+
+        Returns:
+            TYPE: Description
+        """
+        return Snippet.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        """
+        Update and return an existing `Snippet` instance,
+        given the validated data.
+
+        Args:
+            instance (TYPE): Description
+            validated_data (TYPE): Description
+
+        Returns:
+            TYPE: Description
+        """
+        instance.title = validated_data.get('title', instance.title)
+        instance.code = validated_data.get('code', instance.code)
+        instance.linenos = validated_data.get('linenos', instance.linenos)
+        instance.language = validated_data.get('language', instance.language)
+        instance.style = validated_data.get('style', instance.style)
+        instance.save()
+        return instance
+
+
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(
+        many=True, queryset=Snippet.objects.all())
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'snippets')
--- snippets/views.py
+++ snippets/views.py
@@ -1,11 +1,11 @@
-from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from django.contrib.auth.models import User
+
 from rest_framework import generics
+from rest_framework import permissions
 
-
-"""
-Using generic class-based views
-"""
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer, UserSerializer
+from snippets.permissions import IsOwnerOrReadOnly
 
 
 class SnippetList(generics.ListCreateAPIView):
@@ -14,6 +14,10 @@ class SnippetList(generics.ListCreateAPIView):
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permissions_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
@@ -22,3 +26,15 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permissions_classes = (permissions.IsAuthenticatedOrReadOnly,
+                           IsOwnerOrReadOnly,)
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
